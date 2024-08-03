4492f42dae1cad57a8066ad2c9444373a0310c38
Nukelimer <112999689+Nukelimer@users.noreply.github.com>
2024-08-03 13:21:34 +0100

    saving changes to fix conflict

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import Header from "./components/Header";
+import Updates from "./components/Updates";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +15,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} relative dark:bg-black flex justify-between`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          <Header />
+          {children}
+          <Updates/>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,31 +1,9 @@
-'use client'
-
-import React, { useEffect, useState } from 'react'
-import LinkPreview from './components/LinkPreview'
-
-const slugs_og = [
-  'ZPRoQcyHY',
-  'ZPRoQGYuR',
-  'ZPRoQEvXT',
-  'ZPRoQoRyS',
-  'ZPRoQXVpp',
-  'ZPRoQWr8o',
-  'ZPRoC1MUJ',
-  'ZPRoQGKTG',
-  'ZPRoQfkWq',
-  'ZPRoQEuLs',
-  'ZPRoQ4rKA',
-]
-const slugs = ['ZPRoQGKTG', 'ZPRoQfkWq', 'ZPRoQEuLs', 'ZPRoQ4rKA']
+import PreviewContainer from "./components/PreviewContainer";
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='grid grid-cols-6 gap-6'>
-        {slugs_og.map((slug) => {
-          return <LinkPreview url={`https://www.tiktok.com/t/${slug}`} />
-        })}
-      </div>
+    <main className="flex min-h-screen items-center w-full ">
+      <PreviewContainer />
     </main>
-  )
+  );
 }
