75c4a0924e2a067fce86b619a5944fb17e32e414
Ben Porter <ben@kloudly.app>
2024-08-23 14:56:28 -0500

    Added dummy data to database fetch so Remi can still view the app

--- app/api/sanity/user/getSanityUser.js
+++ app/api/sanity/user/getSanityUser.js
@@ -14,10 +14,27 @@ export default async function getSanityUser(whopUserData) {
       "latestNicheID": latestNiche->_id
       }`,
     })
-    userData.sanityID = sanityUserData[0].id
-    userData.latestNiche = {
-      name: sanityUserData[0].latestNicheName,
-      id: sanityUserData[0].latestNicheID,
+    if (sanityUserData[0]) {
+      userData.sanityID = sanityUserData[0]?.id
+      userData.latestNiche = {
+        name: sanityUserData[0]?.latestNicheName,
+        id: sanityUserData[0]?.latestNicheID,
+      }
+    } else {
+      console.log('testing only: no sanity user data found')
+      userData = {
+        id: 'user_wowowowowow',
+        name: 'Testy McTesterson',
+        username: 'testytesterson',
+        email: 'test@gmail.com',
+        profile_pic_url:
+          'https://ui-avatars.com/api/?name=test&background=535961&color=fff',
+        sanityID: '737d6fd4-09f2-492c-bccd-5b4681cdbcf0',
+        latestNiche: {
+          name: 'Beauty',
+          id: 'ade20a10-78d3-49cb-af13-f28d2d874a6e',
+        },
+      }
     }
   }
   return userData
