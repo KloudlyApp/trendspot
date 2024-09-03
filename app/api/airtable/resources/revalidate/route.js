b637896e7ddbce02c5db6f4473ce99a42bf79d36
Ben Porter <ben@kloudly.app>
2024-09-03 11:04:43 -0500

    Fixed revalidate route response message and imports

--- app/api/airtable/resources/revalidate/route.js
+++ app/api/airtable/resources/revalidate/route.js
@@ -1,7 +1,8 @@
 import { revalidateTag } from 'next/cache'
+import { NextResponse } from 'next/server'
 
 export default async function GET() {
   const tag = 'getResources'
   revalidateTag(tag)
-  return NextResponse.json('Revalidated tag:', getActiveNiches)
+  return NextResponse.json(`Revalidated tag: ${tag}`)
 }
--- app/api/airtable/updates/revalidate/route.js
+++ app/api/airtable/updates/revalidate/route.js
@@ -1,7 +1,8 @@
 import { revalidateTag } from 'next/cache'
+import { NextResponse } from 'next/server'
 
 export default async function GET() {
   const tag = 'getUpdates'
   revalidateTag(tag)
-  return NextResponse.json('Revalidated tag:', getActiveNiches)
+  return NextResponse.json(`Revalidated tag: ${tag}`)
 }
