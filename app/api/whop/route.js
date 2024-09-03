e6547733c52dfe63750faa7fd4e5ddf881adbe3f
Ben Porter <ben@kloudly.app>
2024-09-03 18:52:17 -0500

    Removed authorization step since it was redundant with whop's oauth code. Logging request urls

--- app/api/whop/route.js
+++ app/api/whop/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import serverGetAuthToken from './server-get-auth-token'
-import serverAuthorizeUser from './authorize/server-authorize-user'
+// import serverAuthorizeUser from './authorize/server-authorize-user'
 import serverGetUser from './user/server-get-user'
 import serverGetAirtableUser from '../airtable/users/server-get-airtable-user'
 import areObjectsEqual from '@/lib/areObjectsEqual'
@@ -13,26 +13,26 @@ export async function GET(request) {
   if (!code) {
     return NextResponse.json({ error: 'No code provided' }, { status: 400 })
   }
-  console.log('authorize user code', code)
+  console.log('authorize user - code', code)
 
   const tokenData = await serverGetAuthToken(code)
-  console.log('authorize user tokenData', tokenData)
+  console.log('authorize user - tokenData', tokenData)
   const accessToken = tokenData.access_token
-  console.log('authorize user accessToken', accessToken)
 
-  // Use the token to check if the user account has an active subscription to this product. If not, direct them to the not-subscribed page.
-  const authorized = await serverAuthorizeUser(accessToken)
-  console.log('authorize user authorized', authorized)
-  if (!authorized) {
-    return NextResponse.redirect(new URL('/not-subscribed', request.url))
-  }
+  //   // Use the token to check if the user account has an active subscription to this product. If not, direct them to the not-subscribed page.
+  // const authorized = await serverAuthorizeUser(accessToken)
+  // console.log('authorize user - authorized', authorized)
+  // if (!authorized) {
+  //   return NextResponse.redirect(new URL('/not-subscribed', request.url))
+  // }
 
-  // If the user passed the previous check they are authorized. Create or update the user entry in Airtable if it doesn't match the Whop user data.
+  // // If the user passed the previous check they are authorized. Create or update the user entry in Airtable if it doesn't match the Whop user data.
   const whopUser = await serverGetUser(accessToken)
-  console.log('authorize user whopUser', whopUser)
+  console.log('authorize user - whopUser', whopUser)
 
+  // Create or update the user entry in Airtable if it doesn't match the Whop user data.
   const airtableUser = await serverGetAirtableUser(whopUser)
-  console.log('authorize user airtableUser', airtableUser)
+  console.log('authorize user - airtableUser', airtableUser)
 
   const updatedAirtableUser = {
     ...airtableUser,
@@ -61,10 +61,12 @@ export async function GET(request) {
     sessionID = updatedAirtableUser.id
   }
 
-  console.log('authorize user sessionID', sessionID)
+  console.log('authorize user - sessionID', sessionID)
   // Now that Airtable has an accurate record of this user, create a session using the Airtable User Record ID and redirect to the app dashboard.
   await createSession(sessionID)
   console.log('created session')
 
+  console.log('authorize user - redirecting to request.url:', request.url)
+  console.log('authorize user - request.nextUrl is:', request.nextUrl)
   return NextResponse.redirect(new URL('/', request.url))
 }
--- middleware.js
+++ middleware.js
@@ -9,6 +9,8 @@ export default async function middleware(request) {
   const isProtectedRoute = protectedRoutes.includes(path)
   const isPublicRoute = publicRoutes.includes(path)
   console.log('middleware - requested path:', path)
+  console.log('middleware - request.url:', request.url)
+  console.log('middleware - request.nextUrl is:', request.nextUrl)
 
   // Users without a session trying to access protected pages will be sent to login
   if (isProtectedRoute) {
