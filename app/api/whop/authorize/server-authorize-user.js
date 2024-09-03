b6d4b04c31a7dd9fcc37ba42ebd1da8229206a7f
Ben Porter <ben@kloudly.app>
2024-09-03 15:34:56 -0500

    Added loop to auth to check all valid product IDs from the ENV variable

--- app/api/whop/authorize/server-authorize-user.js
+++ app/api/whop/authorize/server-authorize-user.js
@@ -1,20 +1,27 @@
 export default async function serverAuthorizeUser(accessToken) {
-  const productID = process.env.NEXT_PUBLIC_REQUIRED_PRODUCT
+  const productIDs =
+    process.env.process.env.NEXT_PUBLIC_REQUIRED_PRODUCT.split(', ')
+  console.log('product ids', productIDs)
+
   const statuses = ['active', 'trialing', 'completed']
   const statusesQuery = statuses.map((status) => `statuses=${status}`).join('&')
 
-  const response = await fetch(
-    `https://api.whop.com/api/v5/me/memberships?${statusesQuery}&product_id=${productID}`,
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
+  for (let i = 0; i < productIDs.length; i++) {
+    const productID = productIDs[i]
+
+    const response = await fetch(
+      `https://api.whop.com/api/v5/me/memberships?${statusesQuery}&product_id=${productID}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
       },
-    },
-  )
+    )
 
-  const data = await response.json()
+    const data = await response.json()
 
-  // Data.data is an array of memberships matching query parameters. The query parameters filter for only active subscriptions to the Required Product, so if there are any memberships returned, the user is authorized.
-  if (data?.data?.length) return true
-  else return false
+    // Data.data is an array of memberships matching query parameters. The query parameters filter for only active subscriptions to the Required Product, so if there are any memberships returned, the user is authorized.
+    if (data?.data?.length) return true
+  }
+  return false
 }
