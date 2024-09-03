8433cf184044745d2a81c23a58bf46310a8ef999
Ben Porter <ben@kloudly.app>
2024-09-03 18:10:40 -0500

    Loop queries by status instead of .join'ing them into one query

--- app/api/whop/authorize/server-authorize-user.js
+++ app/api/whop/authorize/server-authorize-user.js
@@ -1,7 +1,6 @@
 export default async function serverAuthorizeUser(accessToken) {
   const productIDs = process.env.NEXT_PUBLIC_REQUIRED_PRODUCT.split(', ')
   const statuses = ['completed', 'trialing', 'active']
-  const statusesQuery = statuses.map((status) => `statuses=${status}`).join('&')
 
   const response = await fetch(`https://api.whop.com/api/v5/me/memberships`, {
     headers: {
@@ -11,22 +10,28 @@ export default async function serverAuthorizeUser(accessToken) {
   const allMemberships = await response.json()
   console.log('All user memberships', allMemberships)
 
-  for (let i = 0; i < productIDs.length; i++) {
-    const productID = productIDs[i]
+  for (let j = 0; j < statuses.length; j++) {
+    const status = statuses[j]
+    for (let i = 0; i < productIDs.length; i++) {
+      const productID = productIDs[i]
+      console.log(`Status #${j}: ${status}, Product #${i}: ${productID}`)
 
-    const response = await fetch(
-      `https://api.whop.com/api/v5/me/memberships?${statusesQuery}&product_id=${productID}`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
+      const response = await fetch(
+        `https://api.whop.com/api/v5/me/memberships?$statuses=${status}&product_id=${productID}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
         },
-      },
-    )
+      )
 
-    const data = await response.json()
+      const data = await response.json()
 
-    // Data.data is an array of memberships matching query parameters. The query parameters filter for only active subscriptions to the Required Product, so if there are any memberships returned, the user is authorized.
-    if (data?.data?.length) return true
+      console.log(`Matching Memberships: ${data?.data}`)
+
+      // Data.data is an array of memberships matching query parameters. The query parameters filter for only active subscriptions to the Required Product, so if there are any memberships returned, the user is authorized.
+      if (data?.data?.length) return true
+    }
   }
   return false
 }
