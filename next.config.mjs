aca267433eac05e69b18a58dda4e0a0b0e704ddb
Ben Porter <ben@kloudly.app>
2024-09-03 19:14:42 -0500

    Added netlify.toml and nextjs config rules to help with redirect issues.

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,18 @@
 export const images = {
   domains: ['dummyimage.com', 'v5.airtableusercontent.com'], // Add your domain here
 }
+
+export const rewrites = async () => {
+  return [
+    {
+      source: '/:path*',
+      has: [
+        {
+          type: 'host',
+          value: '(?<host>.*)',
+        },
+      ],
+      destination: '/:path*',
+    },
+  ]
+}
