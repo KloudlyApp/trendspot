b0ad42405f8cca6f7adbeba213eca10d89f6c182
Ben Porter <ben@kloudly.app>
2024-09-03 21:41:27 -0500

    Made resources page more flexible to change categories in the future, and configured for the current 3 categories.

--- app/(app)/resources/page.js
+++ app/(app)/resources/page.js
@@ -6,22 +6,22 @@ import CardGroup from '@/app/components/CardGroup'
 import ResourceCard from '@/app/components/ResourceCard'
 
 export default function Resources() {
-  const [resources, setResources] = useState({
-    tiktok: [],
-    marketing: [],
-    legal: [],
-  })
+  const GROUPS = ['TikTok Resources', 'Content Resources', 'Other Resources']
+
+  const [resources, setResources] = useState(
+    Object.fromEntries(GROUPS.map((group) => [group, []])),
+  )
 
   useEffect(() => {
     const fetchResources = async () => {
       const allResources = await getResources()
-      setResources({
-        tiktok: allResources.filter((item) => item.fields.Group === 'tiktok'),
-        marketing: allResources.filter(
-          (item) => item.fields.Group === 'marketing',
-        ),
-        legal: allResources.filter((item) => item.fields.Group === 'legal'),
-      })
+      const groupedResources = GROUPS.reduce((accumulator, group) => {
+        accumulator[group] = allResources.filter(
+          (item) => item.fields.Group === group,
+        )
+        return accumulator
+      }, {})
+      setResources(groupedResources)
     }
 
     fetchResources()
@@ -29,21 +29,14 @@ export default function Resources() {
 
   return (
     <div className='mt-6 lg:mt-8 lg:w-[calc(100vw-28rem)] px-2 lg:h-screen md:gap-10 lg:gap-2 md:h-[50%] md:flex md:flex-col gap-6 flex flex-col'>
-      <CardGroup
-        title={'TikTok Resources'}
-        CardComponent={(props) => <ResourceCard {...props} />}
-        data={resources.tiktok}
-      />
-      <CardGroup
-        title={'Marketing Resources'}
-        CardComponent={(props) => <ResourceCard {...props} />}
-        data={resources.marketing}
-      />
-      <CardGroup
-        title={'Legal Resources'}
-        CardComponent={(props) => <ResourceCard {...props} />}
-        data={resources.legal}
-      />
+      {GROUPS.map((group) => (
+        <CardGroup
+          key={group}
+          title={group}
+          CardComponent={(props) => <ResourceCard {...props} />}
+          data={resources[group]}
+        />
+      ))}
     </div>
   )
 }
