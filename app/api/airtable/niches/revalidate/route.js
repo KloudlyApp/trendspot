7a646fae6f3f18f353b4ae11922bc33e530ed3d8
Ben Porter <ben@kloudly.app>
2024-09-03 21:47:15 -0500

    Added logs to the revalidate routes

--- app/api/airtable/niches/revalidate/route.js
+++ app/api/airtable/niches/revalidate/route.js
@@ -4,5 +4,6 @@ import { NextResponse } from 'next/server'
 export async function POST() {
   const tag = 'getActiveNiches'
   revalidateTag(tag)
+  console.log(`Revalidated tag: ${tag}`)
   return NextResponse.json(`Revalidated tag: ${tag}`)
 }
--- app/api/airtable/updates/revalidate/route.js
+++ app/api/airtable/updates/revalidate/route.js
@@ -4,5 +4,6 @@ import { NextResponse } from 'next/server'
 export async function POST() {
   const tag = 'getUpdates'
   revalidateTag(tag)
+  console.log(`Revalidated tag: ${tag}`)
   return NextResponse.json(`Revalidated tag: ${tag}`)
 }
