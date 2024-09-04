15433275b64d60aa42c7f2bf033deb69f8d86497
Ben Porter <ben@kloudly.app>
2024-09-03 20:10:36 -0500

    Testing: if code is invalid for The Creator Loop, try again on Trendspot

--- app/api/whop/server-get-auth-token.js
+++ app/api/whop/server-get-auth-token.js
@@ -1,19 +1,72 @@
 const serverGetAuthToken = async (code) => {
-  const response = await fetch('https://api.whop.com/v5/oauth/token', {
+  const response_TCL = await fetch('https://api.whop.com/v5/oauth/token', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       code,
-      client_id: process.env.NEXT_PUBLIC_WHOP_CLIENT_ID,
-      client_secret: process.env.WHOP_CLIENT_SECRET,
+      client_id: process.env.WHOP_CLIENT_ID_TCL,
+      client_secret: process.env.WHOP_CLIENT_SECRET_TCL,
       redirect_uri: process.env.WHOP_OAUTH_CALLBACK_URL,
     }),
   })
+  const data_TCL = await response_TCL.json()
+  console.log('serverGetAuthToken - TCL data:', data_TCL)
 
-  const data = await response.json()
-  return data
+  if (!data_TS.access_token) {
+    const response_TS = await fetch('https://api.whop.com/v5/oauth/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        code,
+        client_id: process.env.WHOP_CLIENT_ID_TS,
+        client_secret: process.env.WHOP_CLIENT_SECRET_TS,
+        redirect_uri: process.env.WHOP_OAUTH_CALLBACK_URL,
+      }),
+    })
+    const data_TS = await response_TS.json()
+    console.log('serverGetAuthToken - TS data:', data_TS)
+
+    return data_TS
+  }
+
+  return data_TCL
 }
 
 export default serverGetAuthToken
+
+// async function betterorder() {
+//   const response_TS = await fetch('https://api.whop.com/v5/oauth/token', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       code,
+//       client_id: process.env.WHOP_CLIENT_ID_TS,
+//       client_secret: process.env.WHOP_CLIENT_SECRET_TS,
+//       redirect_uri: process.env.WHOP_OAUTH_CALLBACK_URL,
+//     }),
+//   })
+//   const data_TS = await response_TS.json()
+
+//   if (!data_TS.access_token) {
+//     const response_TCL = await fetch('https://api.whop.com/v5/oauth/token', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         code,
+//         client_id: process.env.WHOP_CLIENT_ID_TCL,
+//         client_secret: process.env.WHOP_CLIENT_SECRET_TCL,
+//         redirect_uri: process.env.WHOP_OAUTH_CALLBACK_URL,
+//       }),
+//     })
+//     const data_TCL = await response_TCL.json()
+//     return data_TCL
+//   }
+// }
