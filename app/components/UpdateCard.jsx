7db1ae7980dddf0b41e74b73f7cdebbfea61d131
Ben Porter <ben@kloudly.app>
2024-09-05 11:31:31 -0500

    FIxed formatting of update card date and link

--- app/components/UpdateCard.jsx
+++ app/components/UpdateCard.jsx
@@ -7,52 +7,46 @@ function UpdateCard({ date, body, img_url, link_url, link_text }) {
   const shortDate = moment(date).format('MMM Do')
 
   return (
-    <div
-      className={`bg-[#262626]  md:my-0 min-w-[300px] h-[300px] w-[300px] rounded-lg justify-center text-white flex `}
-    >
-      <div
-        className={`flex  flex-col gap-4  ${!img_url && 'justify-around my-0 gap-0'}`}
-      >
-        <div className='flex '>
-          {img_url && (
-            <div className=' h-[90px] max-h-28 flex items-center justify-center overflow-hidden lg:flex-1 w-full rounded-t-lg'>
-              <a
-                href={img_url}
-                target='_blank'
-                rel='noopener noreferrer'
-                className='w-full'
-              >
-                <Image
-                  src={img_url}
-                  height={100}
-                  width={100}
-                  alt='image'
-                  className='w-full hover:scale-110 hover:opacity-80 transition-all'
-                />
-              </a>
-            </div>
-          )}
-        </div>
-
-        <p
-          className={`w-[90%] text-white overflow-y-scroll px-2 ml-4 text-pretty ${img_url ? 'max-h-[7rem]' : 'max-h-[12rem]'} `}
-        >
-          {body}
-        </p>
-
-        <div className='flex justify-between p-6'>
-          <p className='message-white min-w-20'>{shortDate}</p>
-          {link_url && link_text ?
+    <div className='bg-[#262626] md:my-0 min-w-[300px] h-[300px] w-[300px] rounded-lg text-white flex flex-col'>
+      <div className='flex-1 flex flex-col'>
+        {img_url && (
+          <div className='h-[90px] max-h-28 flex items-center justify-center overflow-hidden w-full rounded-t-lg'>
             <a
-              href={link_url}
-              className='text-[#857FFF] text-right hover:underline hover:scale-105 transition-all'
+              href={img_url}
               target='_blank'
               rel='noopener noreferrer'
+              className='w-full'
             >
-              {link_text}
+              <Image
+                src={img_url}
+                height={100}
+                width={100}
+                alt='image'
+                className='w-full hover:scale-110 hover:opacity-80 transition-all'
+              />
             </a>
-          : <div></div>}
+          </div>
+        )}
+
+        <div
+          className={`w-[90%] overflow-y-scroll px-2 ml-4 mt-6 ${img_url ? 'max-h-[8rem]' : 'max-h-[13rem]'} `}
+        >
+          <p className='text-white text-pretty'>{body}</p>
         </div>
+      </div>
+
+      <div className='flex justify-between items-center p-4 mt-auto'>
+        <p className='message-white min-w-20'>{shortDate}</p>
+        {link_url && link_text ?
+          <a
+            href={link_url}
+            className='text-[#857FFF] text-right hover:underline hover:scale-105 transition-all'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {link_text}
+          </a>
+        : <div></div>}
       </div>
     </div>
   )
